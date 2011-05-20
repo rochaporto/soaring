a1e296c07669542055f771d248278a290a40886d
Ricardo Rocha <rocha.porto@gmail.com>
2011-05-20 20:26:29 +0200

    added test addition to google fusion tables

--- python/crawler.py
+++ python/crawler.py
@@ -1,12 +1,33 @@
 import logging
+import urllib
+import urllib2
 
 from google.appengine.api.taskqueue import Task
 from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import appdata
 from igc import FlightReader, FlightExporter, FlightFetcher
 
-class NetcoupeHandler(RequestHandler):
+class CommonHandler(RequestHandler):
+
+    gAuthUri = "https://www.google.com/accounts/ClientLogin"
+
+    fusionTablesUri = "http://www.google.com/fusiontables/api/query"
+
+    def __init__(self):
+        None
+
+    def gAuth(self, username, password, service, accountType):
+        authData = urllib.urlencode(
+                {"Email": username, "Passwd": password, "service": service, 
+                "accountType": accountType})
+        authReq = urllib2.Request(self.gAuthUri, data=authData)
+        authResp = urllib2.urlopen(authReq).read()
+        authDict = dict(x.split("=") for x in authResp.split("\n") if x)
+        return authDict["Auth"]
+
+class NetcoupeHandler(CommonHandler):
 
     def get(self):
         logging.info("NetcoupeHandler: started")
@@ -14,15 +35,24 @@ class NetcoupeHandler(RequestHandler):
         task = Task(url="/crawler/netcoupe/worker", params={"url": url})
         task.add("flightprocess")
 
-class NetcoupeWorker(RequestHandler):
+class NetcoupeWorker(CommonHandler):
+
+    def __init__(self):
+        self.authToken = None
 
     def post(self):
         url = self.request.get('url')
         logging.info("NetcoupeWorker: processing flight :: url=%s" % url)
-        logging.debug("NetcoupeWorker: fetching flight :: url=%s" % url)
+        if self.authToken is None:
+            self.authToken = self.gAuth("rocha.porto", appdata.password, "fusiontables", "HOSTED_OR_GOOGLE")
         reader = FlightReader( FlightFetcher(url).fetch() )
         exporter = FlightExporter(reader.flight)
-        logging.debug(exporter.toFusionTable(111))
+        req = urllib2.Request(self.fusionTablesUri,
+                urllib.urlencode({"sql": exporter.toFusionTable(872803)}),
+                {"Authorization": "GoogleLogin auth=%s" % self.authToken,
+                "Content-Type": "application/x-www-form-urlencoded"})
+        resp = urllib2.urlopen(req)
+        print resp
 
 def main():
     app = WSGIApplication([
